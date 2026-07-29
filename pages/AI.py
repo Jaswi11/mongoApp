75b22748cafee4c9f8842201fa15ed053d5f808a
Jaswi11 <jaswikumari06@gmail.com>
2026-07-29 05:48:06 -0400

    Update AI.py

--- pages/AI.py
+++ pages/AI.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
